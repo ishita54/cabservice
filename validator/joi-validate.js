479cdfa12ef56ad7ea63038321b2cd407140baa8
Telusko150 <ishitapanwar1996@gmail.com>
2019-02-25 12:18:30 +0530

    done

--- validator/joi-validate.js
+++ validator/joi-validate.js
@@ -4,7 +4,7 @@ const Joi = require('joi');
 const joi_validate=(res,data,schema)=>{
    let result = Joi.validate(data,schema)
         if(result.error){
-            response.errorResponse(res,400,error.details[0].message)
+            response.errorResponse(res,400,result.error.details[0].message)
             return false;
         }
         else{
